236a1eb5497bd3c5632a044a4c8db9e609e1b746
Nic Roberts <dominic.cyril.roberts@gmail.com>
2022-08-03 20:44:26 -0400

    Refactored boilerplate

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tour-of-heroes'),
+      dir: require('path').join(__dirname, './coverage/muse-shack'),
       subdir: '.',
       reporters: [
         { type: 'html' },
